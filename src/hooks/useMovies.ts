9a5da36770534dde10e23f9f637630992039b2bd
Ivan <devdnia@gmail.com>
2022-12-29 10:43:01 +0100

    useMovies and HomeScreen

--- src/hooks/useMovies.ts
+++ src/hooks/useMovies.ts
@@ -1,17 +1,47 @@
 import { useEffect, useState } from 'react';
 import movieDB from '../api/movieDB';
-import { MovieDBNowPlaying, Movie } from '../interfaces/movieInterface';
+import { Movie, MovieDBResponse } from '../interfaces/movieInterface';
+
+interface MoviesState {
+    nowPlaying: Movie[];
+    popular: Movie[];
+    topRated: Movie[];
+    upcoming: Movie[];
+}
 
 
 export const useMovies = () => {
 
     const [ isLoading, setIsLoading ] = useState(true);
-    const [peliculasEnCine, setPeliculasEnCine] = useState<Movie[]>([])
+    const [ moviesState, setMoviesState] = useState<MoviesState>({
+        nowPlaying:[],
+        popular:[],
+        topRated:[],
+        upcoming :[],
+    })
+
 
     const getMovies = async () => {
 
-        const resp = await movieDB.get<MovieDBNowPlaying>('/now_playing');
-        setPeliculasEnCine( resp.data.results );
+       const nowPlayingPromise  = movieDB.get<MovieDBResponse>('/now_playing');
+       const popularPromise     = movieDB.get<MovieDBResponse>('/popular');
+       const topRatedPromise    = movieDB.get<MovieDBResponse>('/top_rated');
+       const upcomingPromise    = movieDB.get<MovieDBResponse>('/upcoming');
+        
+       const resps = await Promise.all([ 
+        nowPlayingPromise, 
+        popularPromise, 
+        topRatedPromise, 
+        upcomingPromise 
+        ]);
+        
+        setMoviesState({
+            nowPlaying: resps[0].data.results,
+            popular: resps[1].data.results,
+            topRated: resps[2].data.results,
+            upcoming: resps[3].data.results,
+        })
+
 
         setIsLoading(false);
     }
@@ -22,7 +52,7 @@ export const useMovies = () => {
     }, [])
 
     return {
-        peliculasEnCine,
+        ...moviesState,
         isLoading,
     }
 }
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,36 +1,57 @@
 import React from 'react';
-import { ActivityIndicator, Dimensions, View } from 'react-native';
+import { ActivityIndicator, Dimensions, ScrollView, View } from 'react-native';
 import { useSafeAreaInsets } from 'react-native-safe-area-context';
 import Carousel from 'react-native-snap-carousel';
 import { MoviePoster } from '../componets/MoviePoster';
 import { useMovies } from '../hooks/useMovies';
+import { HorizontalSlider } from '../componets/HorizontalSlider';
 
-const { width: windowWidth, height: windowHeight } = Dimensions.get('window');
+const { width: windowWidth } = Dimensions.get('window');
 
 export const HomeScreen = () => {
 
-    const { peliculasEnCine, isLoading } = useMovies();
+    const { nowPlaying, popular, topRated, upcoming, isLoading } = useMovies();
     const { top } = useSafeAreaInsets()
 
-    if( isLoading ) {
+    if (isLoading) {
         return (
-            <View style={{ flex: 1, justifyContent: 'center', alignContent:'center'}}>
-                <ActivityIndicator color='red' size={100}/>
+            <View style={{ flex: 1, justifyContent: 'center', alignContent: 'center' }}>
+                <ActivityIndicator color='red' size={100} />
             </View>
         )
     }
-    
-    return (
-        <View style={{ marginTop: top + 20 }}> 
-            {/* <MoviePoster movie={ peliculasEnCine[0] }/> */}
 
-            <Carousel 
-                data={ peliculasEnCine }
-                renderItem={ ({ item }: any ) => <MoviePoster movie={ item }/> }
-                sliderWidth={ windowWidth }
-                itemWidth= { 300 }
-                vertical= { false }
-            />
-        </View>
+    return (
+
+        <ScrollView>
+            <View style={{ marginTop: top + 20 }}>
+                {/* <MoviePoster movie={ peliculasEnCine[0] }/> */}
+
+                {/* Carousel Principal */}
+                <View style={{ height: 440 }}>
+                    <Carousel
+                        data={nowPlaying}
+                        renderItem={({ item }: any) => <MoviePoster movie={item} />}
+                        sliderWidth={windowWidth}
+                        itemWidth={300}
+                        vertical={false}
+                    />
+                </View>
+
+                {/* Peliculas populares */}
+                <HorizontalSlider
+                    movies={popular}
+                    title="Populares"
+                />
+                <HorizontalSlider
+                    movies={topRated}
+                    title="Top Rated"
+                />
+                <HorizontalSlider
+                    movies={upcoming}
+                    title="Upcoming"
+                />
+            </View>
+        </ScrollView>
     )
 }
